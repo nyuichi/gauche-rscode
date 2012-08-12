11921e2c86b121e89b745736227317f03b35e878
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2012-08-12 19:45:20 +0900

    refactor polys and add test

--- rscode.scm
+++ rscode.scm
@@ -1,159 +1,180 @@
 (define-module rscode
-  (use srfi-27)
+  (use gauche.uvector)
   (export
     make-rscode
     rs-encode
     rs-decode
-    encode-string
-    decode-string
+    rs-encode-string
+    rs-decode-string
   ))
 
 (select-module rscode)
 
+;;; Utilities
 
-(define (make-polynomial deg)
+(define-syntax swap!
+  (syntax-rules ()
+    ((_ a b)
+     (let ((tmp a))
+       (set! a b)
+       (set! b tmp)))))
+
+
+;;; Polynomial
+
+(define (make-poly deg)
   (make-list (+ deg 1) 0))
 
-(define (polynomial-deg poly)
+(define (poly-deg poly)
   (- (length poly) 1))
 
-(define (polynomial-copy poly)
+(define (poly-copy poly)
   (list-copy poly))
 
-(define (ref-polynomial poly i)
-  (ref poly (- (polynomial-deg poly) i)))
+(define (poly-ref poly i)
+  (ref poly (- (poly-deg poly) i)))
 
-(define (remove-first-zeros list)
-  (if (null? list)
-      list
-      (if (zero? (car list))
-	  (remove-first-zeros (cdr list))
-	  list)))
-
-(define (shrink-polynomial poly)
-  (reverse (remove-first-zeros (reverse poly))))
-
-(define (polynomial= poly1 poly2)
-  (equal? (shrink-polynomial poly1)
-	  (shrink-polynomial poly2)))
-
-(define (get-real-deg poly)
-  (- (length (shrink-polynomial poly)) 1))
-
-(define (polynomial-zero? poly)
+(define (poly-zero? poly)
   (null? poly))
 
+(define (poly-shrink poly)
+  (define (remove-first-zeros list)
+    (if (null? list)
+	list
+	(if (zero? (car list))
+	    (remove-first-zeros (cdr list))
+	    list)))
+  (reverse (remove-first-zeros (reverse poly))))
 
+(define (poly-expand poly n)
+  (if (poly-zero? poly)
+      (make-list n 0)
+      (cons (car poly)
+	    (poly-expand (cdr poly) (- n 1)))))
+
+(define (poly-add-offset offset poly)
+  (if (zero? offset)
+      poly
+      (poly-add-offset (- offset 1) (cons 0 poly))))
+
+(define (poly= poly1 poly2)
+  (equal? (poly-shrink poly1)
+	  (poly-shrink poly2)))
+
+(define (poly-real-deg poly)
+  (- (length (poly-shrink poly)) 1))
+
+
+;;; Misc
+
+(define (max-length-polys lists)
+  (apply max (map length lists)))
+
+(define (expand-polys lists)
+  (let ((len (max-length-polys lists)))
+    (map (lambda (lst) (poly-expand lst len)) lists)))
+
+
+
+;;; GF(2)
+
+;;; 位数が2の有限体の値は0,1の２つ. それにn次の原始多項式の根8個を添加して拡大体としているので
+;;; 値は a[7]*A^7+a[6]*A^6+a[5]*A^5+a[4]*A^4+a[3]*A^3+a[2]*A^2+a[1]*A^1+a[0]*A^0
+;;; となる(Aは根の一つ). a[0]~a[7]はもちろん0か1. だからnbitの整数として表せる.
+;;; ...という認識. 
 
 (define-class <galois-field-2> ()
-  ((max-exp :init-keyword :max-exp)
-   (exp2val :init-keyword :exp2val) 
-   (val2exp :init-keyword :val2exp)))
+  ((size :init-keyword :size)
+   (exp-table :init-keyword :exp-table) 
+   (log-table :init-keyword :log-table)))
 
 (define (get-poly-from-n n)
   (case n
-    ((3) #x03)
-    ((4) #x03)
-    ((8) #x1d)
+    ([3] #x03)
+    ([4] #x03)
+    ([8] #x1d)
     (else (error "Sorry, the specified field is not supported yet."))))
 
+(define (max-exp gf2)
+  (- (~ gf2 'size) 1))
+
 (define (init-galois-field-2 gf2 prim-poly)
-  (let ((tmp 1))
-    (dotimes (i (~ gf2 'max-exp))
-	     (set! (~ gf2 'exp2val i) tmp)
-	     (set! (~ gf2 'val2exp tmp) i)
-	     (set! tmp (logxor (logand (ash tmp 1)
-	     			       (~ gf2 'max-exp))
-	     		       (if (zero? (logand tmp (ash (+ (~ gf2 'max-exp) 1) -1)))
-	     			   0
-	     			   prim-poly))))
-    (set! (~ gf2 'val2exp 0) (~ gf2 'max-exp))))
-
-
+  (let ([x 1]
+	[maximum (max-exp gf2)])
+    (do ((i 0 (+ i 1)))	((>= i maximum))
+      (set! (~ gf2 'exp-table i) x)
+      (set! (~ gf2 'log-table x) i)
+      (set! x (logxor (logand (ash x 1) maximum)
+		      (if (zero? (logand x (ash (~ gf2 'size) -1)))
+			  0
+			  prim-poly))))
+    (set! (~ gf2 'log-table 0) maximum)))
 
 (define (make-galois-field-2 n prim-poly)
-  (let* ((max-exp (- (expt 2 n) 1))
+  (let* ((size (expt 2 n))
 	 (gf2 (make <galois-field-2>
-		:max-exp max-exp
-		:exp2val (make-list max-exp 0)
-		:val2exp (make-list (+ max-exp 1) 0))))
+		:size size
+		:exp-table (make-list size 0)
+		:log-table (make-list size 0))))
     (init-galois-field-2 gf2 prim-poly)
     gf2))
 
+(define-macro (with-gf2 . rest)
+  `(let ((exp-table (~ gf2 'exp-table))
+	 (log-table (~ gf2 'log-table))
+	 (maximum (max-exp gf2)))
+     ,@rest))
 
 (define (gf2-add gf2 . rest)
   (apply logxor rest))
 
 (define (gf2-mul gf2 a b)
-  (if (or (zero? a) (zero? b))
-      0
-      (let* ((exp2val (~ gf2 'exp2val))
-	     (val2exp (~ gf2 'val2exp))
-	     (max-exp (~ gf2 'max-exp))
-	     (index (mod (+ (~ val2exp a) (~ val2exp b)) max-exp)))
-	(~ exp2val index))))
+  (with-gf2
+   (if (or (zero? a) (zero? b))
+       0
+       (let ((index (mod (+ (~ log-table a) (~ log-table b)) maximum)))
+	 (~ exp-table index)))))
 
 (define (gf2-pow gf2 a exp)
-  (if (zero? a)
-      0
-      (begin (set! exp (mod exp (~ gf2 'max-exp)))
-	     (let* ((exp2val (~ gf2 'exp2val))
-		    (val2exp (~ gf2 'val2exp))
-		    (max-exp (~ gf2 'max-exp))
-		    (index (mod (* (~ val2exp a) exp) max-exp)))
-	       (~ exp2val index)))))
+  (with-gf2
+   (if (zero? a)
+       0
+       (let ((index (mod (* (~ log-table a) exp) maximum)))
+	 (~ exp-table index)))))
 
 (define (gf2-div gf2 a b)
-  (if (zero? a)
-      0
-      (if (zero? b)
-	  (error "divide by zero")
-	  (let* ((exp2val (~ gf2 'exp2val))
-		 (val2exp (~ gf2 'val2exp))
-		 (max-exp (~ gf2 'max-exp))
-		 (index (mod (- (~ val2exp a) (~ val2exp b)) max-exp)))
-	    (~ exp2val index)))))
+  (with-gf2
+   (if (zero? a)
+       0
+       (if (zero? b)
+	   (error "divide by zero")
+	   (let ((index (mod (- (~ log-table a) (~ log-table b)) maximum)))
+	     (~ exp-table index))))))
 
 (define (gf2-pow-a gf2 exp)
-  (~ gf2 'exp2val (mod exp (~ gf2 'max-exp))))
+  (with-gf2
+   (~ exp-table (mod exp maximum))))
 
 (define (gf2-log-a gf2 val)
-  (~ gf2 'val2exp val))
-
-(define (add-offset offset list)
-  (if (zero? offset)
-      list
-      (add-offset (- offset 1) (cons 0 list))))
-
-(define (max-length-polys lists)
-  (apply max (map length lists)))
-
-(define (expand-poly list n)
-  (if (null? list)
-      (make-list n 0)
-      (cons (car list)
-	    (expand-poly (cdr list) (- n 1)))))
-
-(define (expand-polys lists)
-  (let ((len (max-length-polys lists)))
-    (map (lambda (lst) (expand-poly lst len)) lists)))
+  (with-gf2
+   (~ log-table val)))
 
 (define (gf2-mul-poly gf2 a b)
   (apply map (lambda rest (apply gf2-add gf2 rest))
 	 (expand-polys
 	  (map (lambda (j offset)
-		 (add-offset offset (map (lambda (i) (gf2-mul gf2 i j)) a)))
+		 (poly-add-offset offset (map (lambda (i) (gf2-mul gf2 i j)) a)))
 	       b
-	       (iota (+ (polynomial-deg b) 1))))))
+	       (iota (+ (poly-deg b) 1))))))
 
 (define (gf2-divmod-poly gf2 a b)
-  (let ((real-b-deg (get-real-deg b)))
+  (let ((real-b-deg (poly-real-deg b)))
     (when (< real-b-deg 0)
 	  (error "Devide by zero poly"))
     (let ((denominator (~ b real-b-deg))
-	  (q (make-polynomial (- (polynomial-deg a) real-b-deg)))
-	  (r (polynomial-copy a)))
-      (do ((i (polynomial-deg q) (- i 1)))
+	  (q (make-poly (- (poly-deg a) real-b-deg)))
+	  (r (poly-copy a)))
+      (do ((i (poly-deg q) (- i 1)))
 	  ((< i 0))
 	(set! (~ q i) (gf2-div gf2 (~ r (+ i real-b-deg)) denominator))
 	(do ((j 0 (+ j 1)))
@@ -173,14 +194,15 @@
   (apply map (lambda args (apply gf2-add gf2 args)) (expand-polys rest)))
 
 (define (gf2-dif-poly gf2 a)
-  (let ((ans (make-polynomial (polynomial-deg a))))
+  (let ((ans (make-poly (poly-deg a))))
     (do ((i 1 (+ i 2)))
-	((> i (polynomial-deg a)))
+	((> i (poly-deg a)))
       (set! (~ ans (- i 1)) (~ a i)))
     ans))
 
 (define (gf2-calc-poly gf2 a b)
-  (apply gf2-add gf2 (map (lambda (c i) (gf2-mul gf2 c (gf2-pow gf2 b i))) a (iota (+ (polynomial-deg a) 1)))))
+  (apply gf2-add gf2
+	 (map (lambda (c i) (gf2-mul gf2 c (gf2-pow gf2 b i))) a (iota (+ (poly-deg a) 1)))))
 
 (define (get-generator-polynomial-for-rs gf2 error-words)
   (fold (lambda (i s)
@@ -189,22 +211,15 @@
 	(list 1)
 	(iota error-words)))
 
-(define-syntax swap!
-  (syntax-rules ()
-    ((_ a b)
-     (let ((tmp a))
-       (set! a b)
-       (set! b tmp)))))
-
 (define (gf2-solve-key-equation gf2 a b)
-  (let ((m (polynomial-copy a))
-	(n (polynomial-copy b))
+  (let ((m (poly-copy a))
+	(n (poly-copy b))
 	(x (list 0))
 	(y (list 1)))
-    (when (< (get-real-deg m) (get-real-deg n))
+    (when (< (poly-real-deg m) (poly-real-deg n))
 	  (swap! n m))
-    (while (and (not (polynomial-zero? n))
-		(>= (get-real-deg n) (get-real-deg y)))
+    (while (and (not (poly-zero? n))
+		(>= (poly-real-deg n) (poly-real-deg y)))
 	   (receive (q r) (gf2-divmod-poly gf2 m n)
 		    (let ((z (gf2-add-poly gf2 (gf2-mul-poly gf2 q y) x)))
 		      (set! x y)
@@ -245,7 +260,7 @@
 (define (rs-decode rscode encoded-words)
   ;;
   (let* ((gf2 (~ rscode 'gf2))
-         (r (polynomial-copy encoded-words))
+         (r (poly-copy encoded-words))
          (num-total-words (~ rscode 'num-total-words))
          (num-error-words (~ rscode 'num-error-words))
          (s (map (lambda (i) (gf2-calc-poly gf2 r (gf2-pow-a gf2 i)))
@@ -259,16 +274,42 @@
              (denom (gf2-mul-poly gf2 x (gf2-dif-poly gf2 sigma))))
 ;        (print "denom:" denom)
         (dotimes (i (- num-total-words 1))
-          (when (zero? (gf2-calc-poly gf2 sigma (gf2-pow-a gf2 (- (~ gf2 'max-exp) i))))
+          (when (zero? (gf2-calc-poly gf2 sigma (gf2-pow-a gf2 (- (max-exp gf2) i))))
             (set! (~ r i) (gf2-add gf2 (~ r i)
                                    (gf2-div gf2
-                                            (gf2-calc-poly gf2 omega (gf2-pow-a gf2 (- (~ gf2 'max-exp) i)))
-                                            (gf2-calc-poly gf2 denom (gf2-pow-a gf2 (- (~ gf2 'max-exp) i))))))))))
+                                            (gf2-calc-poly gf2 omega (gf2-pow-a gf2 (- (max-exp gf2) i)))
+                                            (gf2-calc-poly gf2 denom (gf2-pow-a gf2 (- (max-exp gf2) i))))))))))
 ;    (drop r num-error-words)))
     (map (cut gf2-log-a gf2 <>) (reverse (drop r num-error-words)))))
 
-(define (encode-string rscode str)
+(define (rs-encode-string rscode str)
   (rs-encode rscode (u8vector->list (string->u8vector str))))
 
-(define (decode-string rscode lst)
+(define (rs-decode-string rscode lst)
   (u8vector->string (list->u8vector (rs-decode rscode lst))))
+
+
+;;; Test
+
+;; (use srfi-27)
+
+;; (define (randomize c)
+;;   (let ((r (make-poly 25))
+;; 	(e (make-poly 25)))
+;;     (do ((i (+ 1 (random-integer 3)) (- i 1)))
+;;     	((<= i 0))
+;;       (set! (~ r (random-integer (polynomial-deg r))) (random-integer 256)))
+;;     (do ((l 0 (+ l 1)))
+;;     	((> l 25))
+;;       (unless (= (~ r l) (~ c l))
+;;     	      (set! (~ e l) (logxor (~ r l) (~ c l)))))
+;;     e))
+
+;; (define (test)
+;;   (let ((rscode (make-rscode 26 19))
+;; 	(sample-data '(122 100 122 100 122 100 122 54 232 180 50 148 126 232 164 2 153 149 6)))
+;;     (dotimes (i 100)
+;;       (if (poly= sample-data
+;; 		 (rs-decode rscode #?=(randomize (rs-encode rscode sample-data))))
+;; 	  (print i "GOOD")
+;; 	  (print i "BAD")))))
